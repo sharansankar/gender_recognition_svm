a35cc2aedab5361fa417e5a1c4a1841d86178700
sharansankar <sharansankar97@gmail.com>
2017-02-20 11:54:18 -0500

    configured praat script on python

--- train_svm.py
+++ train_svm.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.cross_validation import cross_val_score
+from sklearn import metrics
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 import numpy as np
+import pyaudio
+import wave
+import os
+
 
 
 def import_and_clean():
@@ -47,15 +52,60 @@ def parameter_tuning_svm(input_df):
         svc = SVC(kernel='linear', C=optimal_cval, gamma=g)
         scores = cross_val_score(svc, training, training_result, cv=10, scoring='accuracy')
         accuracy_vals.append(scores.mean())
-
-    plt.plot(gamma_vals, accuracy_vals)
-    plt.xlabel('Gamma Values')
-    plt.ylabel('Mean Accuracies')
-    plt.show()
+    #
+    # plt.plot(gamma_vals, accuracy_vals)
+    # plt.xlabel('Gamma Values')
+    # plt.ylabel('Mean Accuracies')
+    # plt.show()
 
     optimal_gamma = gamma_vals[accuracy_vals.index(max(accuracy_vals))]
     print optimal_gamma
 
+    svc = SVC(kernel='linear', C=optimal_cval, gamma=optimal_gamma)
+    svc.fit(training, training_result)
+    testing_predict = svc.predict(testing)
+    print metrics.accuracy_score(testing_predict, testing_result)
+
+def record_audio():
+
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    RECORD_SECONDS = 5
+    WAVE_OUTPUT_FILENAME = "test_audio.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
 if __name__ == '__main__':
-    df =import_and_clean()
-    parameter_tuning_svm(df)
+    # df =import_and_clean()
+    # parameter_tuning_svm(df)
+    #record_audio()
+    os.system('"Praat.app/Contents/MacOS/Praat" --run extract_freq_info.praat')
